80f6864ece8c56d61583f9d0a48f2a414cb23d7c
Jaroslav O <jaroslav.ozevic@nfq.lt>
2015-07-23 14:26:56 +0300

    Another typo

--- ProxyQuery/Doctrine/ProxyQuery.php
+++ ProxyQuery/Doctrine/ProxyQuery.php
@@ -44,9 +44,9 @@ class ProxyQuery extends BaseProxyQuery
         }
 
         // Fix for PostgreSQL
-        $dqlParts = $$this->queryBuilder->getDQLParts();
+        $dqlParts = $this->queryBuilder->getDQLParts();
         if (count($dqlParts['orderBy']) && !count($dqlParts['groupBy'])) {
-            $$this->queryBuilder->resetDQLPart('orderBy');
+            $this->queryBuilder->resetDQLPart('orderBy');
         }
 
         return $this->getFixedQueryBuilder($this->queryBuilder)->getQuery()->execute($params, $hydrationMode);
